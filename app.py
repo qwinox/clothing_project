c9255dca114deff8300bdfda6f6f92d15350b088
qwinox <60475943+qwinox@users.noreply.github.com>
2022-11-03 18:25:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras import utils
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications import EfficientNetB0
-from tensorflow.keras.applications.efficientnet import preprocess_input
 
 import io
 import requests
@@ -12,34 +10,33 @@ import streamlit as st
 import numpy as np
 from PIL import Image
 
-(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+def model_training():
+    (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+    classes = ['футболка', 'брюки', 'свитер', 'платье', 'пальто', 'туфли', 'рубашка', 'кроссовки', 'сумка', 'ботинки']
+    x_train = x_train.reshape(60000, 784)
+    
+    # Векторизованные операции
+    # Применяются к каждому элементу массива отдельно
+    x_train = x_train / 255 
+    y_train = utils.to_categorical(y_train, 10)
+    
+    # Создаем последовательную модель
+    model = Sequential()
+    
+    # Входной полносвязный слой, 800 нейронов, 784 входа в каждый нейрон
+    model.add(Dense(800, input_dim=784, activation="relu"))
+    
+    # Выходной полносвязный слой, 10 нейронов (по количеству типов одежды)
+    model.add(Dense(10, activation="softmax"))
+    model.compile(loss="categorical_crossentropy", optimizer="SGD", metrics=["accuracy"])
 
-classes = ['футболка', 'брюки', 'свитер', 'платье', 'пальто', 'туфли', 'рубашка', 'кроссовки', 'сумка', 'ботинки']
+    history = model.fit(x_train, y_train, 
+                        batch_size=200, 
+                        epochs=100,
+                        validation_split=0.2,
+                        verbose=1)
+    model.save('fashion_mnist_dense.h5')
 
-x_train = x_train.reshape(60000, 784)
-
-# Векторизованные операции
-# Применяются к каждому элементу массива отдельно
-x_train = x_train / 255 
-
-y_train = utils.to_categorical(y_train, 10)
- 
-# Создаем последовательную модель
-model = Sequential()
-# Входной полносвязный слой, 800 нейронов, 784 входа в каждый нейрон
-model.add(Dense(800, input_dim=784, activation="relu"))
-# Выходной полносвязный слой, 10 нейронов (по количеству типов одежды)
-model.add(Dense(10, activation="softmax"))
-
-model.compile(loss="categorical_crossentropy", optimizer="SGD", metrics=["accuracy"])
-
-history = model.fit(x_train, y_train, 
-                    batch_size=200, 
-                    epochs=100,
-                    validation_split=0.2,
-                    verbose=1)
-
-model.save('fashion_mnist_dense.h5')
 
 def preprocess_image(img):
     img = img.resize((28, 28))
@@ -69,6 +66,9 @@ def print_predictions(preds):
     st.write(classes[preds])
 
 st.title('Распознавание одежды на изображениях')
+training = st.button('Обучить сеть')
+if training:
+    model_training()
 img = load_image()
 result = st.button('Распознать изображение')
 if result:
