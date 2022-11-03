dd98a4d5c739966be233ac700f1057b70146e6fe
qwinox <60475943+qwinox@users.noreply.github.com>
2022-11-03 19:26:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -60,8 +60,8 @@ def load_image():
     
 def print_predictions(preds):
     preds = np.argmax(preds)
-    st.write(preds[0])
-    st.write(classes[preds[0]])
+    st.write(preds)
+    st.write(classes[preds])
 
 st.title('Распознавание одежды на изображениях')
 training = st.button('Обучить сеть')
